990764559fdd976bd710eeffc218ef52f4092f59
Han Nguyen <han.nguyen@outlook.com>
2021-12-25 21:11:31 -0600

    used imperial units

--- src/main/java/com/weather/webservice/component/OpenWeatherComponent.java
+++ src/main/java/com/weather/webservice/component/OpenWeatherComponent.java
@@ -16,18 +16,20 @@ public class OpenWeatherComponent {
     @Value("${app.baseUrl}")
     private String baseUrl;
 
+    private String units = "imperial";
+
     public OpenWeatherComponent() {
         this.restTemplate = new RestTemplate();
     }
 
     public CurrentWeather getCurrentWeather(String city) {
-        String url = baseUrl + "weather?q=" + city + "&appid=" + apiKey;
+        String url = baseUrl + "weather?q=" + city + "&appid=" + apiKey + "&units=" + units;
         CurrentWeather currentWeather = restTemplate.getForEntity(url, CurrentWeather.class).getBody();
         return currentWeather;
     }
 
     public CurrentForecast getForecast(Double longitude, Double latitude) {
-        String url = baseUrl + "onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+        String url = baseUrl + "onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=" + units;
         CurrentForecast currentForecast = restTemplate.getForEntity(url, CurrentForecast.class).getBody();
         return currentForecast;
     }
